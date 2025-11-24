b7bc9f5a78c0d8a057ef0b18daa171fa16207685
brandbern <berningbrand9@gmail.com>
2025-11-24 03:16:46 -0500

    Add character count component

---
+++ src/components/CharCount.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { getTextFromHtml } from '../utils/helpers';
+
+export const CharCount = ({ mode, showPreview, state }) => {
+  const charCount = mode === 'wysiwyg' || showPreview
+    ? getTextFromHtml(state.html).length
+    : mode === 'markdown' 
+      ? state.markdown.length 
+      : state.html.length;
+
+  return (
+    <div className="px-2 py-1 bg-gray-50 border-t border-gray-200 flex justify-end">
+      <span className="text-xs text-gray-500">
+        {charCount} chars
+      </span>
+    </div>
+  );
+};
+
